7c608938c5e349dbc58192190ca16d2e6779dab7
Júnior Nunes <denisonprobable13@gmail.com>
2021-05-31 23:58:44 -0300

    Created theme.ts with basic colors

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+    colors: {
+        primary: '#5636D3',
+        secondary: '#FF872C',
+        secondary_light: '#FF872C49',   
+        success: '#12A454',
+        success_light: '#12A4547f',
+        attention: '#E83F5B',
+        attention_light: '#E83F5B7f',
+        shape: '#FFFFFF',
+        title: '#363F5F',
+        text: '#969CB2',
+        background: '#F0F2F5'
+    }
+
+}
