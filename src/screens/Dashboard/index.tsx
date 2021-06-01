8ee42fac3ffc1cb93cb96d692d51e40ecc621983
Júnior Nunes <denisonprobable13@gmail.com>
2021-05-31 23:20:04 -0300

    Created basic screen

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
 
-import { Container } from './styles'
+import { Container, Title } from './styles'
 
 export function Dashboard(){
     return (
         <Container>
-            <Text>Dashboard</Text>
+            <Title>Dashboard</Title>
         </Container>
     );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,5 +4,10 @@ export const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
-    background-color: #999;
+`;
+
+export const Title = styled.Text`
+    font-size: 24px;
+    font-weight: bold;
+    color: purple;
 `;
