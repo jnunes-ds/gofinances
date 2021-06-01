7bd5fc5e1343c0f5d3b71c9b8702cb3d4b80a91e
Júnior Nunes <denisonprobable13@gmail.com>
2021-05-31 21:34:21 -0300

    styled-components library installed with typing

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export function Dashboard(){
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Dashboard</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
