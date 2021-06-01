15b8b7ff22b33af3110a37e38ef3738f07a1e040
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-01 16:01:02 -0300

    Creating Header

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -2,14 +2,10 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
-    align-items: center;
-
     background-color: ${ ({ theme }) => theme.colors.background };
 `;
 
-export const Title = styled.Text`
-    font-family: ${ ({ theme }) => theme.fonts.bold };
-    font-size: 24px;
-    color: ${ ({ theme }) => theme.colors.title };
+export const Header = styled.View`
+    width: 100%;
+    background-color: ${ ({ theme }) => theme.colors.primary };
 `;
