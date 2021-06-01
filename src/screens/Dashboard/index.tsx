39d35925cdb738bb7f76ec2b41acfd5a29cdea84
Júnior Nunes <denisonprobable13@gmail.com>
2021-05-31 21:48:40 -0300

    Creating first component with styled-components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import { Container } from './styles'
+
 export function Dashboard(){
     return (
-        <View style={styles.container}>
+        <Container>
             <Text>Dashboard</Text>
-        </View>
+        </Container>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
