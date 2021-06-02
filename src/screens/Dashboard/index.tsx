07b502ce016f8a1acdc4363b9e72732259ca1555
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-02 15:01:18 -0300

    Created user components in Header.

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -2,14 +2,29 @@ import React from 'react';
 
 import { 
     Container,
-    Header
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
  } from './styles'
 
 export function Dashboard(){
     return (
         <Container>
             <Header>
-                
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/68449430?v=4' }}/>
+                        
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Júnior</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
             </Header>
         </Container>
     );
