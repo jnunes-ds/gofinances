38406de6b723d8e5590aa2b3cee12817a7fbb08a
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-03 19:00:11 -0300

    Creating Button Component

---
+++ src/Components/Form/Button/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TouchableOpacityProps } from 'react-native';
+
+import {
+    Container,
+    Title,
+} from './styles';
+
+interface Props extends TouchableOpacityProps{
+    title: string;
+}
+
+export function Button({ title }: Props){
+    return (
+        <Container>
+            <Title>
+                { title }
+            </Title>
+        </Container>
+    );
+}
