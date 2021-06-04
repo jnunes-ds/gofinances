777b8e8f9dfc23399d9528352ee835492b21371d
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-04 02:43:09 -0300

    Pre-modal styling of CategorySelect completed.

--- src/Components/Form/CategorySelect/styles.ts
+++ src/Components/Form/CategorySelect/styles.ts
@@ -1,8 +1,26 @@
 import styled from "styled-components/native";
 import { Feather } from '@expo/vector-icons';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View``;
+export const Container = styled.TouchableOpacity.attrs({
+    activeOpacity: 0.7
+})`
+    background-color: ${ ({ theme }) => theme.colors.shape };
 
-export const Category = styled.Text``;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
 
-export const Icon = styled(Feather)``;
+    border-radius: 5px;
+    padding: 18px 16px;
+`;
+
+export const Category = styled.Text`
+    font-family: ${ ({ theme }) => theme.fonts.regular };
+    font-size: ${RFValue(14)}px;
+`;
+
+export const Icon = styled(Feather)`
+    font-size: ${RFValue(20)}px;
+    color: ${ ({ theme }) => theme.colors.text }
+`;
