053095c07491973cf1a28bd2a643095c02396158
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-04 15:35:49 -0300

    start with modal

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -66,7 +66,7 @@ export function Register(){
             </Form>
 
             <Modal>
-                <CategorySe lect />  
+                <CategorySelect />  
             </Modal>
 
         </Container>
