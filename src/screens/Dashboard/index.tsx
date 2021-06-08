1a24ebfbbf7c418dfbf37da77c0af193950a3591
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-08 00:32:17 -0300

    Button modifications (react-native-gesture-handler)

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -11,11 +11,12 @@ import {
     User,
     UserGreeting,
     UserName,
+    LogoutButton,
     Icon,
     HighlightCards,
     Transactions,
     Title,
-    TransactionList
+    TransactionList,
  } from './styles'
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 
@@ -85,7 +86,9 @@ export function Dashboard(){
                         </User>
                     </UserInfo>
 
-                    <Icon name="power"/>
+                    <LogoutButton onPress={() => {}}>
+                        <Icon name="power"/>
+                    </LogoutButton>
                 </UserWrapper>
             </Header>
 
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,6 +4,8 @@ import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons';
 import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
 
+import { BorderlessButton } from 'react-native-gesture-handler';
+
 import { DataListProps } from './index';
 
 export const Container = styled.View`
@@ -62,6 +64,8 @@ export const UserName = styled.Text`
     font-size: ${RFValue(18)}px;
     font-family: ${ ({ theme }) => theme.fonts.bold };
 `;
+
+export const LogoutButton = styled(BorderlessButton)``;
 
 export const Icon = styled(Feather)`
     color: ${ ({ theme }) => theme.colors.secondary };
