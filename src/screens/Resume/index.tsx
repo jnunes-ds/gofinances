e50f0cbfcdb9cc073e0e58f6bf4c6cf5c6a4b205
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-11 18:48:00 -0300

    using useFocusEffect

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { ActivityIndicator } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { VictoryPie } from 'victory-native';
@@ -24,6 +24,7 @@ import {
     LoadContainer
 } from './styles';
 import { categories } from '../../utils/categories';
+import { useFocusEffect } from '@react-navigation/native';
 
 interface TransactionData{
     type: 'positive' | 'negative';
@@ -123,6 +124,10 @@ export function Resume(){
     useEffect(() => {
         loadData();
     }, [selectedDate])
+
+    useFocusEffect(useCallback(() => {
+        loadData();
+    }, []))
 
     return (
         <Container>
