3eb87ce318869d935ee02bf30f73b224afc9168a
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-13 19:15:13 -0300

    Styling login screen

--- src/screens/SingIn/index.tsx
+++ src/screens/SingIn/index.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
 
+import AppleSvg from '../../assets/apple.svg';
+import GoogleSvg from '../../assets/google.svg';
+import LogoSvg from '../../assets/logo.svg';
+import { RFValue } from 'react-native-responsive-fontsize';
+
 import{
-    Container
+    Container,
+    Header,
+    TitleWrapper,
+    Title,
+    SingInTitle,
+    Footer,
 } from './styles';
 
 export function SingIn(){
     return (
         <Container>
-            
+            <Header>
+                <TitleWrapper>
+                    <LogoSvg 
+                        width={RFValue(120)}
+                        height={RFValue(68)}
+                    />
+                    <Title>
+                        Controle suas{'\n'}
+                        finanças de forma{'\n'}
+                        muito simples
+                    </Title>
+                </TitleWrapper>
+                <SingInTitle>
+                    Faça seu login com{'\n'}
+                    uma das contas abaixo
+                </SingInTitle>
+            </Header>
+            <Footer>
+
+            </Footer>
         </Container>
     );
 }
--- src/screens/SingIn/styles.ts
+++ src/screens/SingIn/styles.ts
@@ -1,3 +1,47 @@
 import styled from "styled-components/native";
+import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+    flex: 1;
+`;
+
+export const Header = styled.View`
+    width: 100%;
+    height: 70%;
+
+    background-color: ${({ theme }) => theme.colors.primary};
+
+    justify-content: flex-end;
+    align-items: center;
+`;
+
+export const TitleWrapper = styled.View`
+    align-items: center;
+`;
+export const Title = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.medium};
+    color: ${({theme}) => theme.colors.shape};
+    font-size: ${RFValue(30)}px;
+
+    text-align: center;
+
+    margin-top: ${RFValue(45)}px;
+`;
+
+export const SingInTitle = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    color: ${({theme}) => theme.colors.shape};
+    font-size: ${RFValue(16)}px;
+
+    text-align: center;
+
+    margin-top: ${RFValue(80)}px;
+    margin-bottom: ${RFValue(67)}px;
+`;
+
+export const Footer = styled.View`
+    width: 100%;
+    height: 30%;
+
+    background-color: ${({ theme }) => theme.colors.secondary};
+`;
