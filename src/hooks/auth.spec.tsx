3bcddf95ab2740cd17ef8b96216a15b40bc9cfe4
Júnior Nunes <denisonprobable13@gmail.com>
2022-04-21 15:39:32 -0300

    using jest.mock

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,6 +1,22 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import { AuthProvider, useAuth } from './auth';
 
+jest.mock('expo-google-app-auth', () => {
+	return {
+		logInAsync: () => {
+			return {
+				type: 'success',
+				user: {
+					id: '123-456-789',
+					email: 'junior@mail.com',
+					name: 'Junior',
+					photo: 'photo.png'
+				}
+			}
+		}
+	}
+});
+
 describe('Auth Hook', () => {
 	it('Should be able to sign in with google existing account', async () => {
 		const { result } = renderHook(() => useAuth(), {
@@ -10,6 +26,7 @@ describe('Auth Hook', () => {
 		await act(() => result.current.singInWithGoogle());
 
 		expect(result.current.user).toBeTruthy();
+		expect(result.current.user.email).toEqual('junior@mail.com')
 
 	});
 });
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -40,14 +40,11 @@ function AuthProvider({ children } : AuthProviderProps){
 
     async function singInWithGoogle(){
         try {
-            console.log('CONSOLE LOG ANTES DO RESULT');
             const result = await Google.logInAsync({
                 iosClientId: '1057683706020-3i7hu719j0ivps93u60qrgt9uddim77b.apps.googleusercontent.com',
                 androidClientId: '1057683706020-7s62n190ohu12o8ughnmmtvl4e5lrtu0.apps.googleusercontent.com',
                 scopes: ['profile', 'email'],
             });
-
-            console.log('RESULTADO DA AUTENTICAÇÃO =>', result);
 
             if(result.type === 'success'){
                 const userLogged = {
