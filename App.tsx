6e2916d7840e1dfecee3f35887382fb23030757a
Júnior Nunes <denisonprobable13@gmail.com>
2022-07-02 18:38:29 -0300

    splash screen added to project

--- App.tsx
+++ App.tsx
@@ -1,53 +1,68 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 
-import React from 'react';
-import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components';
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import * as SplashScreen from "expo-splash-screen";
 
-import AppLoading from 'expo-app-loading';
+import AppLoading from "expo-app-loading";
 
-import 'react-native-gesture-handler';
-import 'intl';
-import 'intl/locale-data/jsonp/pt-BR'
+import "react-native-gesture-handler";
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
-  Poppins_700Bold
-} from '@expo-google-fonts/poppins'
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
-import theme from './src/global/styles/theme';
-import { Routes } from './src/routes';
+import theme from "./src/global/styles/theme";
+import { Routes } from "./src/routes";
 
-import { AuthProvider, useAuth } from './src/hooks/auth';
+import { AuthProvider, useAuth } from "./src/hooks/auth";
 
 export default function App() {
+  7;
+  const [loading, setLoading] = useState(true);
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
-    Poppins_700Bold
+    Poppins_700Bold,
   });
 
   const { userStorageLoading } = useAuth();
 
-  if(!fontsLoaded || userStorageLoading){
-    return <AppLoading />
+  useEffect(() => {
+    if (!fontsLoaded || userStorageLoading) {
+      (async () => {
+        await SplashScreen.preventAutoHideAsync();
+      })();
+    } else {
+      (async () => {
+        await SplashScreen.hideAsync();
+      })();
+      setLoading(false);
+    }
+  }, [fontsLoaded, userStorageLoading]);
+
+  if (loading) {
+    return <AppLoading />;
   }
 
   return (
     <ThemeProvider theme={theme}>
-        <StatusBar
-          animated={true}
-          showHideTransition="fade"  
-          translucent={true}
-          backgroundColor="transparent"
-          barStyle="light-content"
-        />
-        <AuthProvider>
-          <Routes />
-        </AuthProvider>
+      <StatusBar
+        animated={true}
+        showHideTransition="fade"
+        translucent={true}
+        backgroundColor="transparent"
+        barStyle="light-content"
+      />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
-
